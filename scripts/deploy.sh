267630364a441ecef2bc88264722f17591baf44e
a-nthonymai <Anthony.Mai@hotmail.com>
2018-09-06 16:23:19 +1000

    Added some scripts for deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+set -x
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+set +x
+
+set -x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
+
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
+java -jar target/${NAME}-${VERSION}.jar
